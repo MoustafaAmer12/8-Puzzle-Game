f69ea2a43d9bb20256e943942a8b810cf65f407f
mahmoud1512 <120992403+mahmoud1512@users.noreply.github.com>
2024-10-27 22:22:17 +0300

    integration with Solver

---
+++ Algorithms/DFS.py
@@ -0,0 +1,70 @@
+import time
+
+from Algorithms.Solver import Solver
+class DFS (Solver):
+    # initializing parameters of DFS
+    def __init__(self, initial_state: str):
+        super().__init__(initial_state)
+        self.parent={}   # parent map to store parent, child relationships
+        self.maxDepth=0   # getting Max Depth of search (m)
+        self.expandedNodes=0   # counter for expanded nodes
+        self.DepthMap={}       # depth map storing node , Depth relationships
+
+    def solve(self):
+
+        startTime=round(time.time()*1000)
+
+        frontier=[]
+        explored=set()
+        frontier.append(int(self.initial_state))
+        self.parent[int(self.initial_state)]=-1
+        self.DepthMap[int(self.initial_state)]=0
+
+
+        while frontier:
+
+            current=frontier.pop()
+            explored.add(current)
+            if(self.DepthMap[current]>self.maxDepth):    #updating max depth
+                self.maxDepth=self.DepthMap[current]
+
+            self.expandedNodes=self.expandedNodes+1
+
+            if(super().GoalTest(current)):    #Goal test
+                ans=[]
+                while(self.parent[current]!= -1):    # constructing path
+                    ans.append(current)
+                    current=self.parent[current]
+                ans.append(current)    #append start state
+                ans.reverse()          # reverse path to get it in right order
+                endTime=round(time.time()*1000)
+                return ans, len(ans)-1, self.expandedNodes, self.maxDepth, endTime-startTime   # returning data
+
+
+
+            Stringified_State=str(current)     # stringifiy state
+            if(Stringified_State.find('0')==-1):         # solving problem of missing zero
+                Stringified_State='0'+Stringified_State
+
+            index=Stringified_State.find('0')
+            children=super().generateChildren(Stringified_State,index)   # generating children
+
+            for child in children:
+                if (int(child) in explored):
+                    continue
+                elif (int(child) in frontier):
+                    continue
+                else:
+                    self.DepthMap[int(child)]=self.DepthMap[int(current)]+1   #depth of child
+                    self.parent[int(child)]=current    #setting parent
+                    frontier.append(int(child))        # appending to frontier
+
+
+
+
+def run_solver(solver: Solver):
+    result = solver.solve()
+    print(result)
+
+dfs_solver = DFS("718654230")
+run_solver(dfs_solver)  # Example initial state
--- Algorithms/Solver.py
+++ Algorithms/Solver.py
@@ -1,7 +1,7 @@
 class Solver:
     def __init__(self, initial_state :str):
         self.initial_state = initial_state
-        self.steps = list()
+
 
     def check_solvable(self):
         count = 0
@@ -12,6 +12,43 @@ class Solver:
                     self.initial_state[i] != '0'):
                     count += count
         return (count % 2 == 0)
+
+    # To test goal in solution
+    def GoalTest(self,state):
+        return state == 12345678
+    # To generate children of a state
+    def generateChildren(self,stringified_state, index):
+        up, down, left, right = index - 3, index + 3, index - 1, index + 1
+        children = []
+
+        # Convert the string to a list to allow modifications
+        state_list = list(stringified_state)
+
+        # moving up
+        if up >= 0:
+            new_state = state_list[:]
+            new_state[index], new_state[up] = new_state[up], '0'
+            children.append(''.join(new_state))
+
+        # Move left
+        if left >= 0 and index % 3 != 0:  # mod checks for points on the corner
+            new_state = state_list[:]
+            new_state[index], new_state[left] = new_state[left], '0'
+            children.append(''.join(new_state))
+
+        # Move right
+        if right <= 8 and (index + 1) % 3 != 0:
+            new_state = state_list[:]
+            new_state[index], new_state[right] = new_state[right], '0'
+            children.append(''.join(new_state))
+
+            # moving down
+        if down <= 8:
+            new_state = state_list[:]
+            new_state[index], new_state[down] = new_state[down], '0'
+            children.append(''.join(new_state))
+
+        return children
 
     def map(self):
         pass
